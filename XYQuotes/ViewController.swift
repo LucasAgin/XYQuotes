35dea4d12a9a5c6b949a4110992fa767a7339da7
aginls <aginls@miamioh.edu>
2017-05-08 21:04:38 -0400

    Initial commit

---
+++ XYQuotes/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  XYQuotes
+//
+//  Created by Lucas Agin on 3/24/17.
+//  Copyright © 2017 LucasAgin. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+  
+
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var quoteField: UITextField!
+    @IBOutlet weak var senderField: UITextField!
+    @IBOutlet weak var responseLabel: UILabel!
+    var postDict: [String: AnyObject] = [:]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self.postDict)
+        getQuotes()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendQuote(_ sender: Any) {
+        addQuote(quote: quoteField.text ?? "Invalid Quote", sender: senderField.text ?? "Invalid Sender")
+        responseLabel.text = "Succesfully added quote \"" + quoteField.text! + "\" by " + senderField.text! + "."
+        responseLabel.textColor = UIColor.green
+        quoteField.text = ""
+        senderField.text = ""
+    }
+
+   
+    func addQuote(quote: String, sender : String) {
+        let quoteRef = FIRDatabase.database().reference(withPath: "Quotes")
+        let quoteLine : NSDictionary = ["Quote": quote, "addedby": sender]
+        quoteRef.childByAutoId().setValue(quoteLine)
+    }
+
+    func getQuotes () {
+        let quoteRef = FIRDatabase.database().reference(withPath: "Quotes")
+        let refHandle = quoteRef.observe(FIRDataEventType.value, with: { (snapshot) in
+            self.postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            // ...
+            print(self.postDict)
+            
+        })
+    }
+    
+    
+    
+}
+
